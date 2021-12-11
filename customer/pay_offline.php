65695bc0ef1c29db5c82d2ee92fee8f2ef020f5e
Caleb Gbadegbe <eponzy2002@gmail.com>
2021-12-11 00:56:18 +0000

    'zeus'

---
+++ customer/pay_offline.php
@@ -0,0 +1,53 @@
+<center><!-- center Starts -->
+
+<h1> Pay OffLine Using Method  </h1>
+
+<p class="text-muted" >
+
+If you have any questions, please feel free to <a href="../contact.php" >contact us,</a> our customer service center is working for you 24/7.
+
+</p>
+
+</center><!-- center Ends -->
+
+<hr>
+
+
+<div class="table-responsive" ><!-- table-responsive Starts -->
+
+<table class="table table-bordered table-hover table-striped" ><!-- table table-bordered table-hover table-striped Starts -->
+
+<thead><!-- thead Starts -->
+
+<tr>
+
+<th> Bank Account Details </th>
+
+<th> Mobile money Cash Details: </th>
+
+<th> Western Union Details: </th>
+
+</tr>
+
+</thead><!-- thead Ends -->
+
+<tbody><!-- tbody Starts -->
+
+<tr>
+
+<td> Bank Name:United Bank OF Africa Account No:03333333494645 Branch Code:0342 Branch Name:Aflao Branch	 </td>
+
+<td> merchant ID:7452986 Mobile No:0548784993, Name:Let Your Light Shine Ent </td>
+
+<td> Full Name:Let Your LightShine, Mobile No:70000150, Name:Your Light, Country:Ghana, N.I.C No:011234567
+</td>
+
+
+</tr>
+
+</tbody><!-- tbody Ends -->
+
+
+</table><!-- table table-bordered table-hover table-striped Ends -->
+
+</div><!-- table-responsive Ends -->
